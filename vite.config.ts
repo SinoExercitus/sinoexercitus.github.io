2fb00daa48d9784e1c8dfc2014f369b5d270f385
Donglin Yu <yu.jerry@msn.com>
2024-09-12 16:48:59 +1200

    Update ts to js

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    mimeTypes: {
-      'text/javascript': ['.ts']
-    }
-  },
   // base should be '/' for root domain
   base: './',
 });
